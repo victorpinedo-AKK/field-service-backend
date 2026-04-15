cf71298f7265156acb894d5ad2b60be3c878de14
victorpinedo-AKK <victor.pinedo@akkinstallations.com>
2026-04-15 00:42:45 -0600

    Use path-style R2 URLs for Android upload compatibility

--- src/config/r2.ts
+++ src/config/r2.ts
@@ -4,6 +4,7 @@ import { env } from "./env";
 export const r2 = new S3Client({
   region: "auto",
   endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: env.R2_ACCESS_KEY_ID,
     secretAccessKey: env.R2_SECRET_ACCESS_KEY,
