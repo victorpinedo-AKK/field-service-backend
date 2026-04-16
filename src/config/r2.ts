6c7e337341dc47e902d72863bd6e910bb2660a26
victorpinedo-AKK <victor.pinedo@akkinstallations.com>
2026-04-16 03:13:09 -0600

    Add log endpoint for photo upload for hotshots

--- src/config/r2.ts
+++ src/config/r2.ts
@@ -1,9 +1,14 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { env } from "./env";
 
+const r2Endpoint = `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`;
+
+console.log("R2 ACCOUNT ID:", JSON.stringify(env.R2_ACCOUNT_ID));
+console.log("R2 ENDPOINT:", r2Endpoint);
+
 export const r2 = new S3Client({
   region: "auto",
-  endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: r2Endpoint,
   forcePathStyle: true,
   credentials: {
     accessKeyId: env.R2_ACCESS_KEY_ID,
